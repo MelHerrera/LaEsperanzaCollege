bd1c2d90bcc01f91b76a9f65334cd522e0a2efb0
Melkin Herrera mendoza <melkinantonioh@hotmail.com>
2020-11-14 14:32:56 -0600

    Agregar icono de la camara

--- app/src/main/java/com/app/laesperanzacollege/fragmentos/AdminFragment.kt
+++ app/src/main/java/com/app/laesperanzacollege/fragmentos/AdminFragment.kt
@@ -14,6 +14,7 @@ import androidx.fragment.app.Fragment
 import com.app.laesperanzacollege.*
 import com.app.laesperanzacollege.Preferencias
 import com.app.laesperanzaedm.model.Usuario
+import kotlinx.android.synthetic.main.fragment_admin1.*
 import kotlinx.android.synthetic.main.fragment_admin1.view.*
 import java.util.*
 
