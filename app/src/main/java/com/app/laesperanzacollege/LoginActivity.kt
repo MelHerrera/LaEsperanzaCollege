4e7cc9145f449eaf26b23f681032f9be6a7c996e
Melkin Herrera mendoza <melkinantonioh@hotmail.com>
2020-12-21 14:36:32 -0600

    validaciones en perfil usuario y admin

--- app/src/main/java/com/app/laesperanzacollege/LoginActivity.kt
+++ app/src/main/java/com/app/laesperanzacollege/LoginActivity.kt
@@ -1,7 +1,9 @@
 package com.app.laesperanzacollege
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.app.laesperanzadao.UsuarioDAO
@@ -67,5 +69,9 @@ class LoginActivity : AppCompatActivity() {
         }
 
         return true
+    }
+
+    fun seguir(view: View) {
+            startActivity(Intent(Intent.ACTION_VIEW,Uri.parse(getString(R.string.face_uri))))
     }
 }
